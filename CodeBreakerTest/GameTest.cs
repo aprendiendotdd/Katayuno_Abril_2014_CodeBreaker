eb2e623905cae30ad229e7de0e2e395926c7564b
joaqfer <joaquin.fdez.ruiz@gmail.com>
2014-04-29 23:53:22 +0200

    Test Verde ReturnMoreXWhenTwoOrMoreCharsCodeAndPasswordAreTheSame

--- CodeBreakerTest/GameTest.cs
+++ CodeBreakerTest/GameTest.cs
@@ -6,21 +6,35 @@ namespace CodeBreakerTest
   [TestFixture]
   public class GameTest
   {
+    private Game _game;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _game = new Game();
+    }
+
     [Test]
     public void ReturnEmptyStringWhenPasswordCharsNotMatchCodeChars()
     {
-      var game = new Game();
-      Assert.AreEqual("", game.GetResultGame("RRRR", "AAAA"));
+      Assert.AreEqual("", _game.GetResultGame("RRRR", "AAAA"));
     }
 
     [Test]
     public void ReturnXWhenACharCodeAndPasswordIsTheSame()
     {
-      var game = new Game();
-      Assert.AreEqual("X", game.GetResultGame("RRRR", "RAAA"));
-      Assert.AreEqual("X", game.GetResultGame("RRRR", "ARAA"));
-      Assert.AreEqual("X", game.GetResultGame("RRRR", "AARA"));
-      Assert.AreEqual("X", game.GetResultGame("RRRR", "AAAR"));
+      Assert.AreEqual("X", _game.GetResultGame("RRRR", "RAAA"));
+      Assert.AreEqual("X", _game.GetResultGame("RRRR", "ARAA"));
+      Assert.AreEqual("X", _game.GetResultGame("RRRR", "AARA"));
+      Assert.AreEqual("X", _game.GetResultGame("RRRR", "AAAR"));
+    }
+
+    [Test]
+    public void ReturnMoreXWhenTwoOrMoreCharsCodeAndPasswordAreTheSame()
+    {
+      Assert.AreEqual("XX", _game.GetResultGame("RAMV", "RANN"));
+      Assert.AreEqual("XXX", _game.GetResultGame("RAMV", "RAMN"));
+      Assert.AreEqual("XXXX", _game.GetResultGame("RAMV", "RAMV"));
     }
   }
 }
